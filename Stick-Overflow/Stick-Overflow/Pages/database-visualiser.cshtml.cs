d45d126616abaf0dd6038be5bc1ce2990cb4a619
alex-211 <alessandropor@icloud.com>
2025-04-10 13:32:17 +0200

    Inizio progetto
    Creato DB
    Inizializzato progetto
    Installato librerie
    Creato pagina di visualizzazione del DB x debug

---
+++ Stick-Overflow/Stick-Overflow/Pages/database-visualiser.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Stick_Overflow.Pages
+{
+    public class database_visualiserModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
